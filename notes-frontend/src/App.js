0c447b83137573e872764a0c222babb8b5f84b56
pedroivoal <pedroial@al.insper.edu.br>
2023-04-27 09:37:05 -0300

    mod

--- notes-frontend/src/App.js
+++ notes-frontend/src/App.js
@@ -2,38 +2,29 @@ import Note from "./components/Note";
 import "./App.css";
 
 function App() {
+  const notes = [
+    {
+      id : 1,
+      title: "Receita de miojo",
+      content:
+        "Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque em uma vasilha e aproveite seu snack :)",
+    },
+    {
+      id : 2,
+      title: "Sorvete de banana",
+      content: "Coloque a banana no congelador e espere.",
+    },
+  ];
+
   return (
     <div className="App">
-      <Note />
+      {notes.map((note) => (
+        <Note key={`note__${note.id}`} title={note.title}>
+          {note.content}
+        </Note>
+      ))}
     </div>
   );
 }
 
 export default App;
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- notes-frontend/src/components/Note/index.js
+++ notes-frontend/src/components/Note/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./index.css";
 
-export default function Note() {
-  return <div>Olá mundo!</div>;
+export default function Note(props) {
+  return (
+    <div className="card">
+      <h3 className="card-title">{props.title}</h3>
+      <div className="card-content">{props.children}</div>
+    </div>
+  );
 }
