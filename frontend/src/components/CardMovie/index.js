aed155636cfe208468a98a6cacfa1d77d22b2ac9
pedroivoal <pedroial@al.insper.edu.br>
2023-05-04 09:02:50 -0300

    card movie

--- frontend/src/components/CardMovie/index.js
+++ frontend/src/components/CardMovie/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./index.css";
 import capa from "../../img/Gone Girl.png"
 
-function CardMovie() {
+function CardMovie(props) {
   return (
     <div className="card-movie">
-      <h2 className="card-movie-title">Título Filme</h2>
+      <h2 className="card-movie-title">{props.title}</h2>
       <img className="card-movie-img" src={capa} alt="Nome Filme" />
       <p className="card-movie-description">Descrição do filme</p>
     </div>  
